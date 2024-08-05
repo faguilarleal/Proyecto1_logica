b1f463be58b941b788a8ef4d7adf400a65201892
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-08-05 10:37:22 -0600

    Update proyecto1.py

--- proyecto1.py
+++ proyecto1.py
@@ -3,5 +3,4 @@
 def leerArchivo():
     with open("BL-Flickr-Images-Book.csv", "r", encoding= "utf-8") as archivo: 
         contenido = archivo.readlines()
-        contenidoSinEncabezado = contenido[1:]
-        return contenidoSinEncabezado
+        return contenido
